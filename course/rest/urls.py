f2b7fe03ddc1cfb7dcd0acf50c9dda84a3fe5266
Zuanng <lequang01122004@gmail.com>
2025-09-03 17:07:17 +0700

    chore:

--- course/rest/urls.py
+++ course/rest/urls.py
@@ -6,8 +6,8 @@ router = routers.DefaultRouter()
 router.register(r'breeds', views.BreedViewSet)
 router.register(r'cats', views.CatViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+# Kết nối API bằng automatic URL routing
+# Ngoài ra, bao gồm login URLs cho browsable API
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
