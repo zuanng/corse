3fed3da3c6d04d15b240b94a9667a8506b1e3335
Zuanng <lequang01122004@gmail.com>
2025-09-04 16:38:50 +0700

    feat: serializers

--- course/rest/views.py
+++ course/rest/views.py
@@ -3,14 +3,14 @@ from rest_framework import viewsets
 from rest.serializers import BreedSerializer, CatSerializer
 
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class BreedViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows breeds to be viewed or edited.
     """
     authentication_classes = (SessionAuthentication, BasicAuthentication)
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
     queryset = Breed.objects.all()
     serializer_class = BreedSerializer
@@ -20,7 +20,7 @@ class CatViewSet(viewsets.ModelViewSet):
     API endpoint that allows cats to be viewed or edited.
     """
     authentication_classes = (SessionAuthentication, BasicAuthentication)
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
